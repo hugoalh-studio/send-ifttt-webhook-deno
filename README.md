1290c5fc85199c77b0e2dedfa426db5dd4c1ad2c
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-06-05 17:24:21 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,84 @@
+# Send IFTTT Webhook (Deno)
+
+![License](https://img.shields.io/static/v1?label=License&message=MIT&style=flat-square "License")
+[![GitHub Repository](https://img.shields.io/badge/Repository-181717?logo=github&logoColor=ffffff&style=flat-square "GitHub Repository")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno)
+[![GitHub Stars](https://img.shields.io/github/stars/hugoalh-studio/send-ifttt-webhook-deno?label=Stars&logo=github&logoColor=ffffff&style=flat-square "GitHub Stars")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno/stargazers)
+[![GitHub Contributors](https://img.shields.io/github/contributors/hugoalh-studio/send-ifttt-webhook-deno?label=Contributors&logo=github&logoColor=ffffff&style=flat-square "GitHub Contributors")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno/graphs/contributors)
+[![GitHub Issues](https://img.shields.io/github/issues-raw/hugoalh-studio/send-ifttt-webhook-deno?label=Issues&logo=github&logoColor=ffffff&style=flat-square "GitHub Issues")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr-raw/hugoalh-studio/send-ifttt-webhook-deno?label=Pull%20Requests&logo=github&logoColor=ffffff&style=flat-square "GitHub Pull Requests")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno/pulls)
+[![GitHub Discussions](https://img.shields.io/github/discussions/hugoalh-studio/send-ifttt-webhook-deno?label=Discussions&logo=github&logoColor=ffffff&style=flat-square "GitHub Discussions")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno/discussions)
+[![CodeFactor Grade](https://img.shields.io/codefactor/grade/github/hugoalh-studio/send-ifttt-webhook-deno?label=Grade&logo=codefactor&logoColor=ffffff&style=flat-square "CodeFactor Grade")](https://www.codefactor.io/repository/github/hugoalh-studio/send-ifttt-webhook-deno)
+
+| **Releases** | **Latest** (![GitHub Latest Release Date](https://img.shields.io/github/release-date/hugoalh-studio/send-ifttt-webhook-deno?label=&style=flat-square "GitHub Latest Release Date")) | **Pre** (![GitHub Latest Pre-Release Date](https://img.shields.io/github/release-date-pre/hugoalh-studio/send-ifttt-webhook-deno?label=&style=flat-square "GitHub Latest Pre-Release Date")) |
+|:-:|:-:|:-:|
+| [![GitHub](https://img.shields.io/badge/GitHub-181717?logo=github&logoColor=ffffff&style=flat-square "GitHub")](https://github.com/hugoalh-studio/send-ifttt-webhook-deno/releases) ![GitHub Total Downloads](https://img.shields.io/github/downloads/hugoalh-studio/send-ifttt-webhook-deno/total?label=&style=flat-square "GitHub Total Downloads") | ![GitHub Latest Release Version](https://img.shields.io/github/release/hugoalh-studio/send-ifttt-webhook-deno?sort=semver&label=&style=flat-square "GitHub Latest Release Version") | ![GitHub Latest Pre-Release Version](https://img.shields.io/github/release/hugoalh-studio/send-ifttt-webhook-deno?include_prereleases&sort=semver&label=&style=flat-square "GitHub Latest Pre-Release Version") |
+
+## 📝 Description
+
+A Deno module to send IFTTT webhook.
+
+> **🔗 Other Edition:**
+>
+> - [NodeJS](https://github.com/hugoalh-studio/send-ifttt-webhook-nodejs)
+
+## 📚 Documentation
+
+### Getting Started
+
+- Deno >= v1.34.0
+
+```ts
+/* Either */
+import { ... } from "https://deno.land/x/send-ifttt-webhook/mod.ts";// Named Import
+import * as iftttWebhook from "https://deno.land/x/send-ifttt-webhook/mod.ts";// Namespace Import
+import IFTTTWebhook from "https://deno.land/x/send-ifttt-webhook/mod.ts";// Default Import (Class `IFTTTWebhook`)
+```
+
+### API
+
+#### Class
+
+- ```ts
+  new IFTTTWebhook(key: string, options: IFTTTWebhookConstructorOptions = {}): IFTTTWebhook;
+    .send(options: IFTTTWebhookSendOptions = {}): Promise<Response>;
+    .sendArbitrary(options: Omit<IFTTTWebhookSendOptions, "arbitrary"> = {}): Promise<Response>;
+
+  IFTTTWebhook.send(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "eventName"> = {}): Promise<Response>;
+  IFTTTWebhook.sendArbitrary(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "arbitrary" | "eventName"> = {}): Promise<Response>;
+  ```
+
+#### Function
+
+- ```ts
+  send(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "eventName"> = {}): Promise<Response>;
+  ```
+- ```ts
+  sendArbitrary(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "arbitrary" | "eventName"> = {}): Promise<Response>;
+  ```
+
+#### Interface / Type
+
+- ```ts
+  interface IFTTTWebhookConstructorOptions {
+    /* Define a default value of whether to trigger with an arbitrary payload. */
+    arbitraryDefault: boolean = false;
+    /* Define a default value of the event name. */
+    eventNameDefault?: string;
+  }
+  ```
+- ```ts
+  interface IFTTTWebhookSendOptions {
+    /* Whether to trigger with an arbitrary payload. */
+    arbitrary: boolean = arbitraryDefault;
+    /* Event name. */
+    eventName: string = eventNameDefault;
+    /* Payload. */
+    payload: object = {};
+  }
+  ```
+
+### Example
+
+```js
+new IFTTTWebhook("my-ifttt-webhook-key", { eventNameDefault: "test" }).sendArbitrary({ payload: { message: "Hello, world!" } })
+```
---
+++ mod.ts
@@ -0,0 +1,195 @@
+const iftttMakerEventNameRegExp = /^[\dA-Za-z_]+$/u;
+const iftttMakerURLRegExp = /^(?:https:\/\/maker\.ifttt\.com\/use\/)?(?<key>(?:[\dA-Za-z][\dA-Za-z_-]*)?[\dA-Za-z])$/u;
+const iftttWebhookSendInit: RequestInit = {
+	headers: {
+		"Content-Type": "application/json",
+		"User-Agent": `Deno/${Deno.version.deno}-${Deno.build.os}-${Deno.build.arch} SendIFTTTWebhook/1.0.0`
+	},
+	method: "POST",
+	redirect: "follow"
+};
+interface IFTTTWebhookConstructorOptions {
+	/**
+	 * @property arbitraryDefault
+	 * @description Define a default value of whether to trigger with an arbitrary payload.
+	 * @default false
+	 */
+	arbitraryDefault?: boolean;
+	/**
+	 * @property eventNameDefault
+	 * @description Define a default value of the event name.
+	 * @default undefined
+	 */
+	eventNameDefault?: string;
+}
+interface IFTTTWebhookSendOptions {
+	/**
+	 * @property arbitrary
+	 * @description Whether to trigger with an arbitrary payload.
+	 * @default arbitraryDefault
+	 */
+	arbitrary?: boolean;
+	/**
+	 * @property eventName
+	 * @description Event name.
+	 * @default eventNameDefault
+	 */
+	eventName?: string;
+	/**
+	 * @property payload
+	 * @description Payload.
+	 * @default {}
+	 */
+	payload?: object;
+}
+/**
+ * @access private
+ * @function $checkEventNamePattern
+ * @param {string} value
+ * @returns {void}
+ */
+function $checkEventNamePattern(value: string): void {
+	if (!iftttMakerEventNameRegExp.test(value)) {
+		throw new SyntaxError(`\`${value}\` is not a valid IFTTT webhook event name!`);
+	}
+}
+/**
+ * @class IFTTTWebhook
+ * @description Use webhook to connect any of the 750+ apps and integrate other services on IFTTT with your DIY projects. You can create Applets that work with any device or app that can receive a web request.
+ */
+class IFTTTWebhook {
+	#arbitraryDefault = false;
+	#eventNameDefault?: string;
+	#key: string;
+	/**
+	 * @constructor
+	 * @description Create a new IFTTT webhook instance.
+	 * @param {string} key Key (`"ifttt-webhook-key"`), or URL (`"https://maker.ifttt.com/use/ifttt-webhook-key"`).
+	 * @param {IFTTTWebhookConstructorOptions} [options={}] Options.
+	 */
+	constructor(key: string, options: IFTTTWebhookConstructorOptions = {}) {
+		if (typeof key !== "string") {
+			throw new TypeError(`Argument \`key\` must be type of string!`);
+		}
+		if (!iftttMakerURLRegExp.test(key)) {
+			throw new SyntaxError(`Argument \`key\` is not a valid IFTTT webhook key!`);
+		}
+		//@ts-ignore
+		this.#key = key.match(iftttMakerURLRegExp).groups.key;
+		if (typeof options.arbitraryDefault === "boolean") {
+			this.#arbitraryDefault = options.arbitraryDefault;
+		} else if (typeof options.arbitraryDefault !== "undefined") {
+			throw new TypeError(`Argument \`options.arbitraryDefault\` must be type of boolean or undefined!`);
+		}
+		if (typeof options.eventNameDefault === "string") {
+			$checkEventNamePattern(options.eventNameDefault);
+			this.#eventNameDefault = options.eventNameDefault;
+		} else if (typeof options.eventNameDefault !== "undefined") {
+			throw new TypeError(`Argument \`options.eventNameDefault\` must be type of string or undefined!`);
+		}
+	}
+	/**
+	 * @method send
+	 * @description Send an IFTTT webhook.
+	 * @param {IFTTTWebhookSendOptions} [options={}] Options.
+	 * @returns {Promise<Response>} Response.
+	 */
+	send(options: IFTTTWebhookSendOptions = {}): Promise<Response> {
+		let arbitrary: boolean = this.#arbitraryDefault;
+		let eventName: string | undefined = this.#eventNameDefault;
+		if (typeof options.arbitrary === "boolean") {
+			arbitrary = options.arbitrary;
+		} else if (typeof options.arbitrary !== "undefined") {
+			throw new TypeError(`Argument \`options.arbitrary\` must be type of boolean or undefined!`);
+		}
+		if (typeof options.eventName === "string") {
+			$checkEventNamePattern(options.eventName);
+			eventName = options.eventName;
+		} else if (typeof options.eventName !== "undefined") {
+			throw new TypeError(`Argument \`options.eventName\` must be type of string or undefined!`);
+		}
+		if (typeof eventName === "undefined") {
+			throw new Error(`Event name is not defined and does not have default value!`);
+		}
+		return fetch(`https://maker.ifttt.com/trigger/${eventName}${arbitrary ? "/json" : ""}/with/key/${this.#key}`, {
+			body: JSON.stringify(options.payload ?? {}),
+			...iftttWebhookSendInit
+		});
+	}
+	/**
+	 * @method sendArbitrary
+	 * @description Send an IFTTT webhook with arbitrary payload.
+	 * @param {Omit<IFTTTWebhookSendOptions, "arbitrary">} [options={}] Options.
+	 * @returns {Promise<Response>} Response.
+	 */
+	sendArbitrary(options: Omit<IFTTTWebhookSendOptions, "arbitrary"> = {}): Promise<Response> {
+		return this.send({
+			...options,
+			arbitrary: true
+		});
+	}
+	/**
+	 * @method send
+	 * @description Send an IFTTT webhook.
+	 * @param {string} key Key (`"ifttt-webhook-key"`), or URL (`"https://maker.ifttt.com/use/ifttt-webhook-key"`).
+	 * @param {string} eventName Event name.
+	 * @param {Omit<IFTTTWebhookSendOptions, "eventName">} [options={}] Options.
+	 * @returns {Promise<Response>} Response.
+	 */
+	static send(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "eventName"> = {}): Promise<Response> {
+		return new this(key).send({
+			...options,
+			eventName
+		});
+	}
+	/**
+	 * @method sendArbitrary
+	 * @description Send an IFTTT webhook with arbitrary payload.
+	 * @param {string} key Key (`"ifttt-webhook-key"`), or URL (`"https://maker.ifttt.com/use/ifttt-webhook-key"`).
+	 * @param {string} eventName Event name.
+	 * @param {Omit<IFTTTWebhookSendOptions, "arbitrary" | "eventName">} [options={}] Options.
+	 * @returns {Promise<Response>} Response.
+	 */
+	static sendArbitrary(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "arbitrary" | "eventName"> = {}): Promise<Response> {
+		return new this(key).sendArbitrary({
+			...options,
+			eventName
+		});
+	}
+}
+/**
+ * @function send
+ * @description Send an IFTTT webhook.
+ * @param {string} key Key (`"ifttt-webhook-key"`), or URL (`"https://maker.ifttt.com/use/ifttt-webhook-key"`).
+ * @param {string} eventName Event name.
+ * @param {Omit<IFTTTWebhookSendOptions, "eventName">} [options={}] Options.
+ * @returns {Promise<Response>} Response.
+ */
+function send(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "eventName"> = {}): Promise<Response> {
+	return new IFTTTWebhook(key).send({
+		...options,
+		eventName
+	});
+}
+/**
+ * @function sendArbitrary
+ * @description Send an IFTTT webhook with arbitrary payload.
+ * @param {string} key Key (`"ifttt-webhook-key"`), or URL (`"https://maker.ifttt.com/use/ifttt-webhook-key"`).
+ * @param {string} eventName Event name.
+ * @param {Omit<IFTTTWebhookSendOptions, "arbitrary" | "eventName">} [options={}] Options.
+ * @returns {Promise<Response>} Response.
+ */
+function sendArbitrary(key: string, eventName: string, options: Omit<IFTTTWebhookSendOptions, "arbitrary" | "eventName"> = {}): Promise<Response> {
+	return new IFTTTWebhook(key).sendArbitrary({
+		...options,
+		eventName
+	});
+}
+export default IFTTTWebhook;
+export {
+	IFTTTWebhook,
+	send,
+	sendArbitrary,
+	type IFTTTWebhookConstructorOptions,
+	type IFTTTWebhookSendOptions
+};
