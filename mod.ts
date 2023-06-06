b03f5719b31cd9bafe3ce37e1b7fed92ba9e94fe
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-06-06 10:55:58 +0800

    Bump version

--- mod.ts
+++ mod.ts
@@ -3,7 +3,7 @@ const iftttMakerURLRegExp = /^(?:https:\/\/maker\.ifttt\.com\/use\/)?(?<key>(?:[
 const iftttWebhookSendInit: RequestInit = {
 	headers: {
 		"Content-Type": "application/json",
-		"User-Agent": `Deno/${Deno.version.deno}-${Deno.build.os}-${Deno.build.arch} SendIFTTTWebhook/1.0.0`
+		"User-Agent": `Deno/${Deno.version.deno}-${Deno.build.os}-${Deno.build.arch} SendIFTTTWebhook/0.1.0`
 	},
 	method: "POST",
 	redirect: "follow"
